a515b165f6e55c5de0ae6f7e342c01ba88b6912a
Peter Nerg <peter.nerg@gmail.com>
2016-02-05 19:41:58 +0100

    created new class PropertyServiceServlet

---
+++ src/main/java/org/dmonix/zookeeper/PropertyServiceServlet.java
@@ -0,0 +1,72 @@
+/**
+ *  Copyright 2016 Peter Nerg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.dmonix.zookeeper;
+
+import static javascalautils.OptionCompanion.Option;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Peter Nerg
+ */
+@WebServlet(name="PropertyService", 
+urlPatterns={"/properties"}, 
+initParams={ @WebInitParam(name="connectString", value="localhost:6181"), @WebInitParam(name="rootPath", value="/etc/properties") }) 
+public class PropertyServiceServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -5954664255975640068L;
+
+	private PropertiesStorageFactory propertiesStorageFactory;
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		
+		propertiesStorageFactory = PropertiesStorageFactory.apply(config.getInitParameter("connectString"));
+		Option(config.getInitParameter("rootPath")).forEach(value -> propertiesStorageFactory.withRootPath(value));
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getOutputStream().write("hello".getBytes());
+	}
+	
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException ,IOException {
+		
+	}
+}
