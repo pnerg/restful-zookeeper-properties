2c8c81e80f920e6c844baf006d6edf1acbc0aee4
Peter Nerg <peter.nerg@gmail.com>
2016-02-06 06:31:50 +0100

    #9 re-implemented listing the property sets

--- src/main/java/org/dmonix/zookeeper/PropertyServiceServlet.java
+++ src/main/java/org/dmonix/zookeeper/PropertyServiceServlet.java
@@ -16,8 +16,11 @@
 package org.dmonix.zookeeper;
 
 import static javascalautils.OptionCompanion.Option;
+import static javascalautils.TryCompanion.Try;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.function.Function;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -27,46 +30,88 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javascalautils.Try;
+import javascalautils.Unit;
+
 /**
  * @author Peter Nerg
  */
-@WebServlet(name="PropertyService", 
-urlPatterns={"/properties"}, 
-initParams={ @WebInitParam(name="connectString", value="localhost:6181"), @WebInitParam(name="rootPath", value="/etc/properties") }) 
+@WebServlet(name = "PropertyService", displayName = "RESTful ZooKeeper Properties", description = "RESTful interface for managing properties stored in ZooKeeper", urlPatterns = {
+		"/properties/*" }, loadOnStartup = 1, initParams = { @WebInitParam(name = "connectString", value = "localhost:6969"), @WebInitParam(name = "rootPath", value = "/etc/properties") })
 public class PropertyServiceServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -5954664255975640068L;
 
 	private PropertiesStorageFactory propertiesStorageFactory;
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
 	 */
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		
+
+		System.out.println(config.getInitParameter("connectString"));
+		System.out.println(config.getInitParameter("rootPath"));
 		propertiesStorageFactory = PropertiesStorageFactory.apply(config.getInitParameter("connectString"));
 		Option(config.getInitParameter("rootPath")).forEach(value -> propertiesStorageFactory.withRootPath(value));
-		
+
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getOutputStream().write("hello".getBytes());
+		String path = Option(req.getPathInfo()).map(p -> p.substring(1)).getOrElse(() -> "");
+
+		Try<List<String>> result = propertiesStorageFactory.create().flatMap(storage -> storage.propertySets());
+		result.map(names -> {
+			Try(() -> resp.getWriter().write(names.toString()));
+			return Unit.Instance;
+		}).recover(new RecoverFunction(resp));
+
 	}
+
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+
 	
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException ,IOException {
-		
+	
+	private static final class RecoverFunction implements Function<Throwable, Unit> {
+		private final HttpServletResponse resp;
+
+		private RecoverFunction(HttpServletResponse resp) {
+			this.resp = resp;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.util.function.Function#apply(java.lang.Object)
+		 */
+		@Override
+		public Unit apply(Throwable t) {
+			Try(() -> {
+				resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				t.printStackTrace(resp.getWriter());
+			});
+			return Unit.Instance;
+		}
+
 	}
 }
